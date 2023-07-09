a76db0d524a982bc401763427e1479b4112b3f36
ArashRmz <arash1.ramzani@gmail.com>
2023-07-09 12:05:22 +0330

    add CameraFollow class

---
+++ Assets/Src/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Src.Scripts
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private Vector3 offset;
+        [SerializeField] private float smoothSpeed = 5f;
+
+        private void Start()
+        {
+            offset = transform.position - target.position;
+        }
+
+        private void LateUpdate()
+        {
+            Quaternion playerRotation = target.rotation;
+            var desiredPosition = target.position +  playerRotation * offset;
+            var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+            
+            transform.LookAt(target);
+        }
+    }
+}
