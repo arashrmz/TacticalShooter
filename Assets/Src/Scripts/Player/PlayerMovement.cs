04ca9d47a8f99b4c8a6580fbf9f8ca2ab3068739
ArashRmz <arash1.ramzani@gmail.com>
2023-07-09 12:05:35 +0330

    update movement and rotation

--- Assets/Src/Scripts/Player/PlayerMovement.cs
+++ Assets/Src/Scripts/Player/PlayerMovement.cs
@@ -7,39 +7,48 @@ namespace Src.Scripts.Player
     {
         [SerializeField] private float moveSpeed = 5f;
         [SerializeField] private float rotateSpeed = 10f;
-
+        
         private void Update()
         {
-            var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            HandleMovement();
+            HandleRotation();
+        }
 
-            //handle movement
+        private void HandleMovement()
+        {
+            var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
             var movement = new Vector3(input.x, 0f, input.y) * moveSpeed;
             // Normalize the direction vector to prevent faster diagonal movements
             if (movement.magnitude > 0)
                 movement.Normalize();
 
-            // Move the player in world space direction without considering rotation
-            transform.Translate(movement * (moveSpeed * Time.deltaTime), Space.World);
-            
+            // Move the player in local space relative to its own rotation
+            transform.Translate(movement * moveSpeed * Time.deltaTime);
+        }
+
+        private void HandleRotation()
+        {
             // Handle rotation based on mouse input or other means (e.g., touch drag)
             Vector3 mousePosition = Input.mousePosition;
             Vector3 playerScreenPos =
                 Camera.main.WorldToScreenPoint(transform.position);
-
+     
             Vector2 offsetMousePos =
                 new Vector2(mousePosition.x - playerScreenPos.x,
                     mousePosition.y - playerScreenPos.y);
-
+                     
             float targetRotationY =
                 Mathf.Atan2(offsetMousePos.x, offsetMousePos.y) *
                 Mathf.Rad2Deg;
 
             Quaternion targetRotation =
-                Quaternion.Euler(0f, targetRotationY, 0f);
+                Quaternion.Euler(0f, targetRotationY , 0f);
+         
+            // transform.rotation= Quaternion.Lerp(transform.rotation,
+            //     targetRotation,
+            //     rotateSpeed * Time.deltaTime);
 
-            transform.rotation = Quaternion.Lerp(transform.rotation,
-                targetRotation,
-                rotateSpeed * Time.deltaTime);
+            transform.rotation = targetRotation;
         }
     }
 }
