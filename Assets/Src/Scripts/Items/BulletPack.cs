11aa802e33b68a6b76e859226b317fbe8f21e523
ArashRmz <arash1.ramzani@gmail.com>
2023-07-10 17:12:06 +0330

    add BulletPack class

---
+++ Assets/Src/Scripts/Items/BulletPack.cs
@@ -0,0 +1,13 @@
+﻿using Src.Scripts.Player;
+using UnityEngine;
+
+namespace Src.Scripts.Items
+{
+    public class BulletPack : Item
+    {
+        protected override void Pickup()
+        {
+            GameObject.FindWithTag("Player").GetComponent<PlayerShooting>().AddAmmo(30);
+        }
+    }
+}
