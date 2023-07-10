66954f4f476d1579260288d0ba2404cb9124445a
ArashRmz <arash1.ramzani@gmail.com>
2023-07-10 15:25:01 +0330

    add EnemyBullet temp class

---
+++ Assets/Src/Scripts/Enemies/EnemyBullet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Src.Scripts.Enemies;
+using UnityEngine;
+
+namespace Src.Scripts.Enemies
+{
+    public class EnemyBullet : MonoBehaviour
+    {
+        private Vector3 _direction; //direction bullet must travel
+        private bool _isShot = false; //if the bullet is shot
+
+        [SerializeField] private float moveSpeed = 10f; //move speed of the bullet
+        [SerializeField] private int damage = 1; //damage given by the bullet
+        [SerializeField] private float timeToLive = 5f;
+
+        public void SetTarget(Transform target)
+        {
+            _direction = (target.position - transform.position).normalized;
+            _isShot = true;
+        }
+
+        private void Update()
+        {
+            if (!_isShot)
+                return;
+
+            MoveBullet();
+            CalculateTimeToLive();
+        }
+
+        //moves the bullet in its direction
+        private void MoveBullet()
+        {
+            // Move the bullet towards the target position with a constant speed
+            transform.Translate(_direction * (moveSpeed * Time.deltaTime));
+        }
+
+        //calculate how much time bullet has to live
+        private void CalculateTimeToLive()
+        {
+            timeToLive -= Time.deltaTime;
+            if (timeToLive <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            //if hit the enemy
+            if (other.CompareTag("Player"))
+            {
+                // other.GetComponent<Enemy>().TakeDamage(damage);
+                Destroy(gameObject);
+            }
+            else if (!other.CompareTag("Enemy"))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
