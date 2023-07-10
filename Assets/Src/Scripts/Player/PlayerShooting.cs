e259159979817aaba876bf511677ed6b107d7f78
ArashRmz <arash1.ramzani@gmail.com>
2023-07-10 16:08:52 +0330

    Update PlayerShooting.cs
    add a test weapon and reload

--- Assets/Src/Scripts/Player/PlayerShooting.cs
+++ Assets/Src/Scripts/Player/PlayerShooting.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
 using Src.Scripts.Enemies;
+using Src.Scripts.Weapons;
 using UnityEngine;
 
 namespace Src.Scripts.Player
@@ -7,13 +10,26 @@ namespace Src.Scripts.Player
     public class PlayerShooting : MonoBehaviour
     {
         [SerializeField] private GameObject bulletPrefab;
-        [SerializeField] private float shootingRate = 0.5f;
         
         private EnemyDetection _enemyDetection;
         private float _currentShootingRate = 0f;
+        private Weapon _testWeapon;
+        private bool _reloading = false;
         
         private void Start()
         {
+            //create a test weapon
+            _testWeapon = new Weapon
+            {
+                name = "Pistol",
+                fireRate = 0.5f,
+                magazineCapacity = 5,
+                currentAmmoInMagazine = 5,
+                totalBullets = 50,
+                damage = 1,
+                reloadTime = 2000
+            };
+            
             _enemyDetection = GetComponent<EnemyDetection>();
             _enemyDetection.OnDetectEnemy += ShootEnemies;
         }
@@ -24,10 +40,18 @@ namespace Src.Scripts.Player
             if(_enemyDetection.VisibleEnemies.Count == 0)
                 return;
 
+            //weapon cooldown
             _currentShootingRate += Time.deltaTime;
-
-            if (_currentShootingRate < shootingRate)
+            if (_currentShootingRate < _testWeapon.fireRate)
                 return;
+            
+            //check bullets in mag
+            if (!_testWeapon.HasAmmoInMag())
+            {
+                Reload();
+                return;
+            }
+                
             //get closest enemy
             var closestDistanceSqr = Mathf.Infinity;
             Enemy closestEnemy = null;
@@ -46,9 +70,22 @@ namespace Src.Scripts.Player
             //shoot at closest enemy
             var bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             bullet.GetComponent<Bullet>().SetTarget(closestEnemy.transform);
+            _testWeapon.Fire();
             _currentShootingRate = 0f;
         }
 
+        async void Reload()
+        {
+            //check if already reloading
+            if (_reloading)
+                return;
+            Debug.Log("Reloading!");
+            _reloading = true;
+            await Task.Delay(_testWeapon.reloadTime);
+            _testWeapon.Reload();
+            Debug.Log("Reloaded!");
+        }
+        
         private void OnDestroy()
         {
             _enemyDetection.OnDetectEnemy -= ShootEnemies;
