eeba0301150b6bfcde5d33371a62ed054bf28077
ArashRmz <arash1.ramzani@gmail.com>
2023-07-10 13:08:04 +0330

    Update EnemyLineOfSight.cs
    add events for player entering and exiting the sight

--- Assets/Src/Scripts/Enemies/EnemyLineOfSight.cs
+++ Assets/Src/Scripts/Enemies/EnemyLineOfSight.cs
@@ -2,14 +2,19 @@
 using UnityEngine;
 
 namespace Src.Scripts.Enemies
-{   
+{
     //checks enemy's line of sight to see if the player is in it
     public class EnemyLineOfSight : MonoBehaviour
     {
         [SerializeField] private float viewDistance = 7f;
         [SerializeField] private float viewAngle = 45f;
-
+        [SerializeField] private LayerMask osbtaclesLayer;
+        
         private Transform _target;
+        private bool _playerInSight; //if player is in sight
+
+        public event Action OnPlayerEnterSight; //event called when player enters the sight
+        public event Action OnPlayerExitSight; //event called when player is no longer in sight
 
         private void Start()
         {
@@ -18,29 +23,46 @@ namespace Src.Scripts.Enemies
 
         private void FixedUpdate()
         {
-            CheckLineOfSight();
+            var isCurrentlyInSight = CheckLineOfSight();
+            
+            if (isCurrentlyInSight && !_playerInSight)
+            {
+                //player just entered the sight
+                _playerInSight = true;
+                OnPlayerEnterSight?.Invoke();
+            }
+            else if (!isCurrentlyInSight && _playerInSight)
+            {
+                //player just exited the sight
+                _playerInSight = false;
+                OnPlayerExitSight?.Invoke();
+            }
         }
-        
+
         //checks to see if target is in view cone
-        private void CheckLineOfSight()
+        private bool CheckLineOfSight()
         {
             if (_target == null)
-                return;
-            
+                return false;
+
             Vector3 directionToPlayer = (_target.position - transform.position).normalized;
-         
+
+            //if player is in view angle
             if (Vector3.Angle(transform.forward, directionToPlayer) <= viewAngle * 0.5f)
-            {    
+            {
                 RaycastHit hit;
-             
-                if(Physics.Raycast(transform.position, directionToPlayer, out hit, viewDistance))
+
+                //if player is in sight
+                if (Physics.Raycast(transform.position, directionToPlayer, out hit, viewDistance, osbtaclesLayer))
                 {
-                    if(hit.collider.CompareTag("Player"))
+                    if (hit.collider.CompareTag("Player"))
                     {
-                        Debug.Log("Player in sight");
+                        return true;
                     }
                 }
             }
+
+            return false;
         }
 
         private void OnDrawGizmosSelected()
@@ -54,7 +76,7 @@ namespace Src.Scripts.Enemies
 
             Gizmos.color = Color.red;
             Gizmos.DrawRay(transform.position, rightBoundary * viewDistance);
-        
+
             Gizmos.color = Color.red;
             Gizmos.DrawRay(transform.position, leftBoundary * viewDistance);
         }
