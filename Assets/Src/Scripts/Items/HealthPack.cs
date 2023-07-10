eabea3d721362ecdb7fcdb7c2f951677e9616f87
ArashRmz <arash1.ramzani@gmail.com>
2023-07-10 17:12:14 +0330

    add HealthPack class

---
+++ Assets/Src/Scripts/Items/HealthPack.cs
@@ -0,0 +1,13 @@
+﻿using Src.Scripts.Player;
+using UnityEngine;
+
+namespace Src.Scripts.Items
+{
+    public class HealthPack : Item
+    {
+        protected override void Pickup()
+        {
+            GameObject.FindWithTag("Player").GetComponent<PlayerManager>().AddHealth(30);
+        }
+    }
+}
