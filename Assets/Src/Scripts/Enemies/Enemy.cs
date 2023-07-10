fa210eac174af6eae970ff27e9c0933a3f22dace
ArashRmz <arash1.ramzani@gmail.com>
2023-07-10 12:18:57 +0330

    add TakeDamage to Enemy
    enemies now take damage

--- Assets/Src/Scripts/Enemies/Enemy.cs
+++ Assets/Src/Scripts/Enemies/Enemy.cs
@@ -1,9 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Src.Scripts.Enemies
 {
     public class Enemy : MonoBehaviour
     {
-        
+        [SerializeField] private int maxHealth;
+        private int _currentHealth;
+
+        private void Start()
+        {
+            _currentHealth = maxHealth;
+        }
+
+        //hit by a bullet
+        public void TakeDamage(int damage)
+        {
+            _currentHealth -= damage;
+            if (_currentHealth <= 0)
+                Die();
+        }
+
+        private void Die()
+        {
+            Destroy(gameObject);
+        }
     }
 }
