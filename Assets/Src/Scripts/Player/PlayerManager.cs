4a11d1eb2675cea4a00b0de54ffd37d7a9701411
ArashRmz <arash1.ramzani@gmail.com>
2023-07-10 12:35:38 +0330

    add PlayerManager class

---
+++ Assets/Src/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Src.Scripts.Player
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        [SerializeField] private int maxHealth = 30;
+
+        private int _currentHealth;
+
+        private void Start()
+        {
+            _currentHealth = maxHealth;
+        }
+
+        //take damage
+        public void TakeDamage(int damage)
+        {
+            _currentHealth -= damage;
+            if (_currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+
+        //player died
+        private void Die()
+        {
+            Debug.Log("You lost!");
+        }
+    }
+}
